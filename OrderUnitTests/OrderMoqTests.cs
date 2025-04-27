2496ef9f59abd0481b98385f9f818580b3d92593
Brian <596833+flizzer@users.noreply.github.com>
2025-04-27 12:32:03 -0400

    creating unit test for GetAllOrders

--- OrderUnitTests/OrderMoqTests.cs
+++ OrderUnitTests/OrderMoqTests.cs
@@ -1,4 +1,6 @@
-//using Moq;
+using OrderAPI;
+using OrderAPI.Data;
+using OrderAPI.Services;
 
 namespace OrderUnitTests;
 
@@ -8,7 +10,34 @@ public class OrderMoqTests
     [Fact]
     public async Task OrderService_GetAllOrders_Returns_Orders()
     {
-        Assert.Fail();
+        //Arrange
+        var mock = new Mock<IOrderService>();
+        mock.Setup(mock => mock.GetAllOrders)
+            .ReturnsAsync(new List<Order>{
+                new Order
+                {
+                    Id = 1,
+                    CustomerId = 2,
+                    OrderNumber = null,
+                    Total = null,
+                    CreatedDate = DateTime.Now(),
+                    Status = OrderStatus.Pending
+                },
+                new Order
+                {
+                   Id = 2,
+                    CustomerId = 50,
+                    OrderNumber = 1001,
+                    Total = 100.00,
+                    CreatedDate = DateTime.Now(),
+                    Status = OrderStatus.Billed 
+                }
+            })
+        
+        //Act
+        var result = await OrderEndpointsV1.GetAllOrders(mock.Object)
+
+        //Assert
     }
 
 }
